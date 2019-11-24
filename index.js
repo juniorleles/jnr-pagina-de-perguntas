766f633726d8bb304bb8396f09db67a534afb7e9
joao leles <jserafim.ext@vr.com.br>
2019-11-24 18:05:52 -0300

    testes

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", 'ejs');
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-   res.render("home");
+    res.render("index");
+
 });
 
 app.listen(8080, ()=>{console.log("App rodando!");});
