6287b660b08c82f5158c1884f5e090a44403fab0
joao leles <jserafim.ext@vr.com.br>
2019-11-26 08:21:42 -0300

    Atualizacoes 25/11

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const Pergunta = require("./database/Pergunta");
+
+//database 
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexecao feita pelo banco de dados!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro)
+    })
 
 app.set("view engine", 'ejs');
 app.use(express.static('public'));
@@ -9,7 +22,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.render("index");
+    Pergunta.findAll({raw: true}).then(perguntas => {
+        res.render("index",{
+            perguntas: perguntas
+        });
+    });
+    
 });
 
 app.get("/perguntar", (req, res) => {
@@ -20,7 +38,13 @@ app.get("/perguntar", (req, res) => {
 app.post("/salvarpergunta",(req, res) => {
    var titulo = req.body.titulo;
    var descricao = req.body.descricao;
-   res.send("Formulário recebido! titulo " + titulo + " " + " descricao " + descricao);
+
+   Pergunta.create({
+       titulo: titulo,
+       descricao: descricao
+   }).then(() => {
+       res.redirect("/");
+   });
 });
 
 
